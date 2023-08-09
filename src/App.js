8772df0f14c6bad43cf148f683c43696c2bb01bf
Hammad Ahmad <hahmad01@i2cinc.com>
2023-08-09 21:37:28 +0500

    useCallback and React.memo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import React, { useCallback } from "react";
+import Button from "./components/UI/Button/Button";
+
 import "./App.css";
+import DemoOutput from "./components/UI/Demo/DemoOutput";
 
 function App() {
-  return <h1>Let's get started!</h1>;
+  const [showParagraph, setShowParagraph] = React.useState(false);
+  const [allowToggle, setAllowToggle] = React.useState(false);
+
+  console.log("APP RUNNING");
+
+  // This function is created everytime the component is re-evaluated and hence a brand new function is created everytime.
+
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = useCallback(() => {
+    setAllowToggle(true);
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
+    </div>
+  );
 }
 
 export default App;
