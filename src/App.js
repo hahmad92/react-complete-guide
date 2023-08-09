6178b026f3db946f9a742b2a5f48d4725a81ea10
Hammad Ahmad <hahmad01@i2cinc.com>
2023-08-09 21:43:30 +0500

    use memo added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState, useMemo } from "react";
 import Button from "./components/UI/Button/Button";
 
 import "./App.css";
-import DemoOutput from "./components/UI/Demo/DemoOutput";
+import DemoList from "./components/UI/Demo/DemoList";
 
 function App() {
-  const [showParagraph, setShowParagraph] = React.useState(false);
-  const [allowToggle, setAllowToggle] = React.useState(false);
+  // const [showParagraph, setShowParagraph] = React.useState(false);
+  // const [allowToggle, setAllowToggle] = React.useState(false);
 
-  console.log("APP RUNNING");
+  // console.log("APP RUNNING");
 
-  // This function is created everytime the component is re-evaluated and hence a brand new function is created everytime.
+  // // This function is created everytime the component is re-evaluated and hence a brand new function is created everytime.
 
-  const toggleParagraphHandler = useCallback(() => {
-    if (allowToggle) {
-      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-    }
-  }, [allowToggle]);
+  // const toggleParagraphHandler = useCallback(() => {
+  //   if (allowToggle) {
+  //     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  //   }
+  // }, [allowToggle]);
 
-  const allowToggleHandler = useCallback(() => {
-    setAllowToggle(true);
+  // const allowToggleHandler = useCallback(() => {
+  //   setAllowToggle(true);
+  // }, []);
+
+  // return (
+  //   <div className="app">
+  //     <h1>Hi there!</h1>
+  //     <DemoOutput show={showParagraph} />
+  //     <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+  //     <Button onClick={allowToggleHandler}>Allow Toggle</Button>
+  //   </div>
+  // );
+
+  const [listTitle, setListTitle] = useState("My List");
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
   }, []);
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
 
   return (
     <div className="app">
-      <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
-      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
-      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }
