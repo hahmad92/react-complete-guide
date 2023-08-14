2939bb6a6eb22d1ee4d9e2084a3236430c5b5222
Hammad Ahmad <hammadawan50@gmail.com>
2023-08-15 00:07:01 +0500

    Data posted to firebase and fethched from firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
+import FIREBASE_URL from "./util/Constants";
 import "./App.css";
 
 function App() {
@@ -12,21 +14,33 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(FIREBASE_URL + "movies.json");
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
       const data = await response.json();
-      const transformedMovies = data.results.map((movie) => {
-        const transformedMovie = {
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-          releaseDate: movie.release_date,
-        };
-        return transformedMovie;
-      });
-      setMovies(transformedMovies);
+
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      // const transformedMovies = data.results.map((movie) => {
+      //   const transformedMovie = {
+      //     id: movie.episode_id,
+      //     title: movie.title,
+      //     openingText: movie.opening_crawl,
+      //     releaseDate: movie.release_date,
+      //   };
+      //   return transformedMovie;
+      // });
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -49,8 +63,27 @@ function App() {
     content = <p>Loading...</p>;
   }
 
+  async function addMovieHandler(movie) {
+    try {
+      const data = await fetch(FIREBASE_URL + "movies.json", {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const response = await data.json();
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
---
+++ src/util/Constants.js
@@ -0,0 +1,3 @@
+const FIREBASE_URL = "https://crack-willow-389614-default-rtdb.firebaseio.com/";
+
+export default FIREBASE_URL;
